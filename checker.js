30770b8523512afb45c624399ae244f896c31903
Madhusree Sakhamuri <madhusree.bellamkonda@gmail.com>
2025-03-17 06:49:54 -0400

    changed the

--- checker.js
+++ checker.js
@@ -1,4 +1,4 @@
 const readlineSync = require('readline-sync');
 let userString = readlineSync.question("Enter a word: ");
 let index = readlineSync.question("Enter an index number: ");
-console.log('index charcter '+ index +' in the String '+ userString +' is = '+ userString[index]);
+console.log('the index charcter '+ index +' in the String '+ userString +' is = '+ userString[index]);
